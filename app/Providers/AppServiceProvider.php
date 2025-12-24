3a8d570a4647d0e03ac667652ffb0ce06d184073
maryamshemshi <maryam.shemshi.1381@gmail.com>
2025-12-24 15:16:43 +0330

    fix: service provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (!Schema::hasTable('users'))
+            return;
         if (!auth()->check()) {
             $user = User::find(1);
             if ($user) {
